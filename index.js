f95fb36b5d4dc0fc3eaaa04afa223ebf8a2ff683
Valentina Fonseca Ulloa <valentinafonsecaulloa8@gmail.com>
2024-04-01 10:46:33 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UT-8">
+    <title>Javascript hello World!</title>
+
+    <script>
+        funcion main() {
+            alert ("hello World");
+        }       
+    </script>
+</head>
+<body>
+
+</body>
+</html> 
